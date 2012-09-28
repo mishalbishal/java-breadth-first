006d983440cd1707a0767ec9dab728de78c260e4
Mishal Rahman <rahman@college.harvard.edu>
2012-09-28 13:16:19 -0400

    adding a node class

---
+++ Main.java
@@ -0,0 +1,32 @@
+class Main
+{
+    public static void main(String args[])
+    {
+        // make a binary tree
+
+        // print the binary tree
+
+        // rejoice! :)
+    }
+
+    public static void binary_print()
+
+    /**
+     * props: http://cslibrary.stanford.edu/110/BinaryTrees.html#java
+     */
+    private static class Node
+    {
+        Node left;
+        Node right;
+        int data;
+
+        // constructor
+        Node(int newData)
+        {
+            left = null;
+            right = null;
+            data = newData;
+        }
+    }
+
+}

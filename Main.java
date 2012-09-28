6ec62bfa1ef5629640b86d4a416888885ccad10d
Mishal Rahman <rahman@college.harvard.edu>
2012-09-28 13:49:48 -0400

    inserting into a tree, and using recursive depth first printing to make sure things are working, also adding a gitignore file for class files

--- Main.java
+++ Main.java
@@ -1,32 +1,53 @@
+import java.util.*;
+
 class Main
 {
+    static boolean debug = true;
     public static void main(String args[])
     {
         // make a binary tree
+        Node root = new Node(1);
+        for (int i = 2; i < 9; i++)
+        {
+            insert(root, i);
+        }
+        if (debug)
+            Node.depth_first_traversal(root); // making sure that things are being inserted
 
         // print the binary tree
-
+        // binaryPrint(root);
         // rejoice! :)
     }
 
-    public static void binary_print()
+    // public static void binaryPrint(Node n)
+    // {
+    //     List<Node> currentBreadth = new ArrayList<Node>();
+    //     currentBreadth.add(n);
+    // }
 
     /**
-     * props: http://cslibrary.stanford.edu/110/BinaryTrees.html#java
+     * props again: http://cslibrary.stanford.edu/110/BinaryTrees.html#java
      */
-    private static class Node
+    public static Node insert(Node node, int data)
     {
-        Node left;
-        Node right;
-        int data;
-
-        // constructor
-        Node(int newData)
+        if (node == null)
         {
-            left = null;
-            right = null;
-            data = newData;
+            node = new Node(data);
         }
+        else
+        {
+            if (data <= node.data)
+            {
+                node.left = insert(node.left, data);
+            }
+            else
+            {
+                node.right = insert(node.right, data);
+            }
+        }
+
+        return node;
     }
+
 
 }

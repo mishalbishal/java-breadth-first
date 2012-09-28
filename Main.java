6edc19e9b19a00ad95615b1e01e4a102609a24d3
Mishal Rahman <rahman@college.harvard.edu>
2012-09-28 14:16:30 -0400

    yay. the idea of using two lists did work :)

--- Main.java
+++ Main.java
@@ -2,11 +2,11 @@ import java.util.*;
 
 class Main
 {
-    static boolean debug = true;
+    static boolean debug = false;
     public static void main(String args[])
     {
         // make a binary tree
-        Node root = new Node(1);
+        Node root = new Node(5);
         for (int i = 2; i < 9; i++)
         {
             insert(root, i);
@@ -15,15 +15,33 @@ class Main
             Node.depth_first_traversal(root); // making sure that things are being inserted
 
         // print the binary tree
-        // binaryPrint(root);
+        breadthFirstPrint(root);
         // rejoice! :)
+        System.out.println("Rejoice :)");
     }
 
-    // public static void binaryPrint(Node n)
-    // {
-    //     List<Node> currentBreadth = new ArrayList<Node>();
-    //     currentBreadth.add(n);
-    // }
+    public static void breadthFirstPrint(Node n)
+    {
+        List<Node> currentBreadth = new ArrayList<Node>();
+        List<Node> nextBreadth = new ArrayList<Node>();
+        currentBreadth.add(n);
+        while(currentBreadth.size() > 0)
+        {
+            Iterator<Node> itr = currentBreadth.iterator();
+            while(itr.hasNext())
+            {
+                Node temp = itr.next();
+                System.out.println(temp);
+                if(temp.left != null)
+                    nextBreadth.add(temp.left);
+                if(temp.right != null)
+                    nextBreadth.add(temp.right);
+            }
+            currentBreadth = nextBreadth;
+            nextBreadth = new ArrayList<Node>(); // reset the new list, garbage collector handles the rest
+            System.out.println("Next Breadth");
+        }
+    }
 
     /**
      * props again: http://cslibrary.stanford.edu/110/BinaryTrees.html#java
